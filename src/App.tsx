a189e87dfb92bb4b2aeef79c5d8bcd32ff7f01da
teller <165670905+TellerSaude@users.noreply.github.com>
2025-08-24 19:09:30 -0300

    chore: adjusted build error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
